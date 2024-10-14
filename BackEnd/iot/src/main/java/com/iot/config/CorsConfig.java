ed053bc0145294452f4c486c9e88d342c9424736
gabrielhbrioto <gabriel.h.brioto@gmail.com>
2024-10-14 06:53:03 -0300

    Rodougit add .git add .git add .!

--- BackEnd/iot/src/main/java/com/iot/config/CorsConfig.java
+++ BackEnd/iot/src/main/java/com/iot/config/CorsConfig.java
@@ -1,3 +1,19 @@
+// package com.iot.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.reactive.config.CorsRegistry;
+// import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+// @Configuration
+// public class CorsConfig implements WebFluxConfigurer {
+
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:3000") // ou qualquer origem que você desejar
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//     }
+// }
 package com.iot.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +26,9 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // ou qualquer origem que você desejar
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("*") // Permite todas as origens, incluindo o Postman
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")  // Permite todos os cabeçalhos
+                .allowCredentials(false); // Desabilita envio de credenciais (ajuste conforme necessário)
     }
 }
