9a372d0403e846ead868458e742c159703bc41d1
gabrielhbrioto <gabriel.h.brioto@gmail.com>
2024-10-28 11:46:59 -0300

    Arquivo index alterado

--- WebApp/scripts/listagem-salas.js
+++ WebApp/scripts/listagem-salas.js
@@ -213,5 +213,5 @@ function inscreverSala() {
 
 document.getElementById('logoutBtn').addEventListener('click', () => {
   sessionStorage.clear(); // Limpa todo o conteúdo do sessionStorage
-  window.location.href = 'http://127.0.0.1:5500/login.html'; // Redireciona para a página de login
+  window.location.href = 'http://127.0.0.1:8081/index.html'; // Redireciona para a página de login
 });
